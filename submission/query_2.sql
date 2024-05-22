78128febf0a1601cac1e7bc0a5eefd746033415a
Aleem Rahil <aleem.rahil@outlook.com>
2024-05-22 00:15:00 -0400

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,5 +1,5 @@
 -- Creating a user devices cumulated table to track user activity using different browsers
-CREATE OR REPLACE TABLE pratzo.user_devices_cumulated (
+CREATE OR REPLACE TABLE aleemrahil84520.user_devices_cumulated (
   user_id BIGINT,                
   browser_type VARCHAR,          
   dates_active ARRAY(DATE),      -- Datelist that tracks how many times a user has been active with a given browser type
