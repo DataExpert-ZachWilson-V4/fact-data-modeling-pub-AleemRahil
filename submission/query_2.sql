2a9d3731b333af8343872522da607e5174a9273c
Aleem Rahil <aleem.rahil@outlook.com>
2024-05-22 00:35:32 -0400

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,10 +1,16 @@
--- Creating a user devices cumulated table to track user activity using different browsers
+-- The purpose of this query is to create a cumulated user activity table by device.
+-- This table will be the result of joining the devices table onto the web_events table,
+-- so that we can get both the user_id and the browser_type.
+-- The schema of this table includes user_id, browser_type, dates_active (an array of dates when the user was active),
+-- and the date field to store the most recent date of activity.
+-- partition key used is date column to allow for more efficient querying when filtering by date
+
 CREATE OR REPLACE TABLE aleemrahil84520.user_devices_cumulated (
-  user_id BIGINT,                
-  browser_type VARCHAR,          
-  dates_active ARRAY(DATE),      -- Datelist that tracks how many times a user has been active with a given browser type
-  date DATE                      
-)
-WITH (
-  FORMAT = 'PARQUET'            
+    user_id bigint,
+    browser_type varchar,
+    dates_active array(date),
+    date date
+) WITH (
+    format = 'PARQUET',
+    partitioning = ARRAY['date']
 )
